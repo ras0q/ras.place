b94c9b402bc30f0440dc20ee12375100302dceee
ras0q <ras0q@users.noreply.github.com>
2023-06-04 02:45:19 +0900

    :sparkles: works page

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,7 +1,46 @@
+'use client'
+
+import { works } from '@/consts'
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const Main = styled.main`
+  padding: 4rem 18rem;
+
+  li {
+    display: frex;
+    list-style: none;
+  }
+`
+
 export default function Works() {
   return (
-    <main>
+    <Main>
       <h1>Works</h1>
-    </main>
+      <ul>
+        {works.map(({ name, link, externalLinks, duration, description }) => (
+          <li key={name}>
+            <div>
+              <div>
+                <h2>
+                  <Link href={link}>{name}</Link>
+                </h2>
+                <p>{duration}</p>
+              </div>
+              <p>{description}</p>
+              {externalLinks && (
+                <ul>
+                  {externalLinks.map(({ href, text }) => (
+                    <li key={text}>
+                      → <Link href={href}>{text}</Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Main>
   )
 }
--- src/consts/index.ts
+++ src/consts/index.ts
@@ -30,3 +30,39 @@ export const accountLinks = [
     text: 'traP blog',
   },
 ]
+
+export const works: {
+  name: string
+  link: string
+  externalLinks?: {
+    href: string
+    text: string
+  }[]
+  duration: string
+  description: string
+}[] = [
+  {
+    name: 'ras.place',
+    link: 'https://ras.place',
+    duration: '2023/03 -',
+    description: 'このポートフォリオ',
+  },
+  {
+    name: 'traPortfolio (WIP)',
+    link: '',
+    duration: '2020/03 -',
+    description: '部員のポートフォリオサイトや各種リンクなどをまとめる外部向けサービス',
+  },
+  {
+    name: 'Qtheme',
+    link: 'https://qtheme.trap.games',
+    externalLinks: [
+      {
+        href: 'https://trap.jp/post/1192',
+        text: '紹介ブログ',
+      },
+    ],
+    duration: 'traP Hackathon 2020 winter',
+    description: '部内SNS『traQ』のカスタムテーマを作成するサービス',
+  },
+]
